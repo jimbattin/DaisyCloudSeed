721a2ac4535fc6de4f43fa6fb043014ad7e1f012
Martin Aspeli <optilude@gmail.com>
2026-01-02 15:46:04 +0000

    Various cleanups to make code more readable and safer

--- petal/CloudSeed/cloudseed.cpp
+++ petal/CloudSeed/cloudseed.cpp
@@ -17,17 +17,51 @@ using namespace daisy;
 using namespace daisysp;
 using namespace terrarium;  // This is important for mapping the correct controls to the Daisy Seed on Terrarium PCB
 
+// Constants
+constexpr size_t AUDIO_BUFFER_SIZE = 48;
+constexpr float OUTPUT_VOLUME_BOOST = 1.2f;
+constexpr int MAX_PRESET_INDEX = 7;
+constexpr int NUM_SWITCHES = 4;
+constexpr float FLOAT_EPSILON = 1e-6f;
+
+// Switch indices for delay line control
+static const int DELAY_LINE_SWITCHES[NUM_SWITCHES] = {
+    Terrarium::SWITCH_1,
+    Terrarium::SWITCH_2,
+    Terrarium::SWITCH_3,
+    Terrarium::SWITCH_4
+};
+
+// Global state structure
+struct PedalState {
+    // Parameters
+    ::daisy::Parameter dryOut;
+    ::daisy::Parameter earlyOut;
+    ::daisy::Parameter mainOut;
+    ::daisy::Parameter delayTime;
+    ::daisy::Parameter diffusion;
+    ::daisy::Parameter tapDecay;
+
+    // Previous parameter values (for change detection)
+    float prevDryOut;
+    float prevEarlyOut;
+    float prevMainOut;
+    float prevDelayTime;
+    float prevDiffusion;
+    float prevTapDecay;
+    float prevNumDelayLines;
+
+    // State
+    bool bypass;
+    int currentPreset;
+    Led led1;
+    Led led2;
+};
+
 // Declare a local daisy_petal for hardware access
 DaisyPetal hw;
-::daisy::Parameter dryOut, earlyOut, mainOut, delayTime, diffusion, tapDecay;
-bool      bypass;
-int       c;
-Led led1, led2;
-
-// Initialize "previous" p values
-float pdryout_value, pearlyout_value, pmainout_value, ptime_value, pdiffusion_value, pnumDelayLines, ptap_decay_value;
-
-CloudSeed::ReverbController* reverb = 0;
+PedalState state;
+CloudSeed::ReverbController* reverb = nullptr;
   
 // This is used in the modified CloudSeed code for allocating 
 // delay line memory to SDRAM (64MB available on Daisy)
@@ -48,134 +82,144 @@ void* custom_pool_allocate(size_t size)
 
 void cyclePreset()
 {
-    c += 1;
-    if ( c > 7 ) {
-        c = 0;
+    state.currentPreset++;
+    if (state.currentPreset > MAX_PRESET_INDEX) {
+        state.currentPreset = 0;
     }
 
     reverb->ClearBuffers();
-        
-    if ( c == 0 ) {
+
+    switch (state.currentPreset) {
+        case 0:
             reverb->initFactoryChorus();
-    } else if ( c == 1 ) {
+            break;
+        case 1:
             reverb->initFactoryDullEchos();
-    } else if ( c == 2 ) {
+            break;
+        case 2:
             reverb->initFactoryHyperplane();
-    } else if ( c == 3 ) {
+            break;
+        case 3:
             reverb->initFactoryMediumSpace();
-    } else if ( c == 4 ) {
+            break;
+        case 4:
             reverb->initFactoryNoiseInTheHallway();
-    } else if ( c == 5 ) {
+            break;
+        case 5:
             reverb->initFactoryRubiKaFields();
-    } else if ( c == 6 ) {
+            break;
+        case 6:
             reverb->initFactorySmallRoom();
-    } else if ( c == 7 ) {
+            break;
+        case 7:
             reverb->initFactory90sAreBack();
-    //} else if ( c == 8 ) {
-    //        reverb->initFactoryThroughTheLookingGlass(); // Only preset that sounds scratchy (using 4-5 delay lines, mono) causes buffer underruns
-    //                                                       //   TODO Try slight modifications to this preset to allow to work
+            break;
+        // case 8:
+        //     reverb->initFactoryThroughTheLookingGlass(); // Only preset that sounds scratchy (using 4-5 delay lines, mono) causes buffer underruns
+        //                                                   //   TODO Try slight modifications to this preset to allow to work
+        default:
+            break;
     }
 }
 
+
+// Audio buffers (moved outside callback to avoid repeated allocation)
+static float audioInputBuffer[AUDIO_BUFFER_SIZE];
+static float audioOutputBuffer[AUDIO_BUFFER_SIZE];
+
+// Helper function to check if float values differ significantly
+inline bool hasChanged(float prev, float current) {
+    return (prev < current - FLOAT_EPSILON) || (prev > current + FLOAT_EPSILON);
+}
 
 // This runs at a fixed rate, to prepare audio samples
 static void AudioCallback(AudioHandle::InputBuffer  in,
                           AudioHandle::OutputBuffer out,
                           size_t                    size)
 {
-    //hw.ProcessAllControls();
     hw.ProcessAnalogControls();
     hw.ProcessDigitalControls();
-    led1.Update();
-    led2.Update();
+    state.led1.Update();
+    state.led2.Update();
 
-    float dryout_value = dryOut.Process();
-    float earlyout_value = earlyOut.Process();
-    float mainout_value = mainOut.Process();
-    float time_value = delayTime.Process();
-    float diffusion_value = diffusion.Process();
-    float tap_decay_value = tapDecay.Process();
+    // Process all parameter values
+    const float dryout_value = state.dryOut.Process();
+    const float earlyout_value = state.earlyOut.Process();
+    const float mainout_value = state.mainOut.Process();
+    const float time_value = state.delayTime.Process();
+    const float diffusion_value = state.diffusion.Process();
+    const float tap_decay_value = state.tapDecay.Process();
 
-    if ((pdryout_value < dryout_value) || ( pdryout_value> dryout_value))
-    {
-      reverb->SetParameter(::Parameter::DryOut, dryout_value);
-      pdryout_value = dryout_value;
+    // Update reverb parameters only when changed
+    if (hasChanged(state.prevDryOut, dryout_value)) {
+        reverb->SetParameter(::Parameter::DryOut, dryout_value);
+        state.prevDryOut = dryout_value;
     }
 
-    if ((pearlyout_value < earlyout_value) || ( pearlyout_value> earlyout_value))
-    {
-      reverb->SetParameter(::Parameter::EarlyOut, earlyout_value);
-      pearlyout_value = earlyout_value;
+    if (hasChanged(state.prevEarlyOut, earlyout_value)) {
+        reverb->SetParameter(::Parameter::EarlyOut, earlyout_value);
+        state.prevEarlyOut = earlyout_value;
     }
 
-    if ((pmainout_value < mainout_value) || ( pmainout_value > mainout_value))
-    {
-      reverb->SetParameter(::Parameter::MainOut, mainout_value);
-      pmainout_value = mainout_value;
+    if (hasChanged(state.prevMainOut, mainout_value)) {
+        reverb->SetParameter(::Parameter::MainOut, mainout_value);
+        state.prevMainOut = mainout_value;
     }
 
-    if ((ptime_value < time_value) || ( ptime_value > time_value))
-    {
-      reverb->SetParameter(::Parameter::LineDecay, time_value);
-      ptime_value = time_value;
-    }
-    if ((pdiffusion_value < diffusion_value) || ( pdiffusion_value > diffusion_value))
-    {
-      reverb->SetParameter(::Parameter::LateDiffusionFeedback, diffusion_value);
-      pdiffusion_value = diffusion_value;
+    if (hasChanged(state.prevDelayTime, time_value)) {
+        reverb->SetParameter(::Parameter::LineDecay, time_value);
+        state.prevDelayTime = time_value;
     }
 
-    if ((ptap_decay_value < tap_decay_value) || ( ptap_decay_value > tap_decay_value))
-    {
-      reverb->SetParameter(::Parameter::TapDecay, tap_decay_value);
-      ptap_decay_value = tap_decay_value;
+    if (hasChanged(state.prevDiffusion, diffusion_value)) {
+        reverb->SetParameter(::Parameter::LateDiffusionFeedback, diffusion_value);
+        state.prevDiffusion = diffusion_value;
+    }
+
+    if (hasChanged(state.prevTapDecay, tap_decay_value)) {
+        reverb->SetParameter(::Parameter::TapDecay, tap_decay_value);
+        state.prevTapDecay = tap_decay_value;
     }
 
     // Delay Line Switches
-    //     - The .Pressed() function below counts an 'ON' switch as pressed.
-    //     - Total number of switches on sets how many delay lines are activated (1 - 5)
-    int switches[4] = {Terrarium::SWITCH_1, Terrarium::SWITCH_2, Terrarium::SWITCH_3, Terrarium::SWITCH_4}; // Can this be moved elsewhere?
-    float numDelayLines = 1.0;
-    for(int i=0; i<4; i++) {
-        if (hw.switches[switches[i]].Pressed()) {
-            numDelayLines += 1.0;
+    // The .Pressed() function below counts an 'ON' switch as pressed.
+    // Total number of switches on sets how many delay lines are activated (1 - 5)
+    float numDelayLines = 1.0f;
+    for (int i = 0; i < NUM_SWITCHES; i++) {
+        if (hw.switches[DELAY_LINE_SWITCHES[i]].Pressed()) {
+            numDelayLines += 1.0f;
         }
     }
-    if (pnumDelayLines != numDelayLines) {
-        //reverb->ClearBuffers();  //TODO is this needed?
+
+    if (hasChanged(state.prevNumDelayLines, numDelayLines)) {
+        // TODO: Determine if ClearBuffers() is needed when changing delay line count
         reverb->SetParameter(::Parameter::LineCount, numDelayLines);
-        pnumDelayLines = numDelayLines;
+        state.prevNumDelayLines = numDelayLines;
     }
 
-    float ins[48];
-    float outs[48];
-    for (size_t i = 0; i < size; i++)
-    {
-        ins[i] = in[0][i];
+    // Copy input to buffer
+    for (size_t i = 0; i < size; i++) {
+        audioInputBuffer[i] = in[0][i];
     }
 
     // (De-)Activate bypass and toggle LED when left footswitch is pressed
-    if(hw.switches[Terrarium::FOOTSWITCH_1].RisingEdge())
-    {
-        bypass = !bypass;
-        led1.Set(bypass ? 0.0f : 1.0f);
+    if (hw.switches[Terrarium::FOOTSWITCH_1].RisingEdge()) {
+        state.bypass = !state.bypass;
+        state.led1.Set(state.bypass ? 0.0f : 1.0f);
     }
 
     // Cycle available models
-    if(hw.switches[Terrarium::FOOTSWITCH_2].RisingEdge())
-    {  
+    if (hw.switches[Terrarium::FOOTSWITCH_2].RisingEdge()) {
         cyclePreset();
     }
 
-    if(!bypass) {
-        reverb->Process(ins, outs, 48);
-        for (size_t i = 0; i < size; i++)
-        {  
-            out[0][i] = outs[i] * 1.2;  // Slight overall volume boost at 1.2
+    if (!state.bypass) {
+        reverb->Process(audioInputBuffer, audioOutputBuffer, AUDIO_BUFFER_SIZE);
+        for (size_t i = 0; i < size; i++) {
+            out[0][i] = audioOutputBuffer[i] * OUTPUT_VOLUME_BOOST;
         }
     } else {
-        for (size_t i = 0; i < size; i++)
-        {  
+        for (size_t i = 0; i < size; i++) {
             out[0][i] = in[0][i];
         }
     }
@@ -183,49 +227,51 @@ static void AudioCallback(AudioHandle::InputBuffer  in,
 
 int main(void)
 {
-    float samplerate;
-
     hw.Init();
-    samplerate = hw.AudioSampleRate();
-    c = 0;
+    const float samplerate = hw.AudioSampleRate();
 
+    // Initialize audio processing libraries
     AudioLib::ValueTables::Init();
     CloudSeed::FastSin::Init();
-    
+
+    // Initialize reverb controller
     reverb = new CloudSeed::ReverbController(samplerate);
     reverb->ClearBuffers();
     reverb->initFactoryChorus();
 
-    //hw.SetAudioBlockSize(4);
+    // Initialize parameters
+    state.dryOut.Init(hw.knob[Terrarium::KNOB_1], 0.0f, 1.0f, ::daisy::Parameter::LINEAR);
+    state.earlyOut.Init(hw.knob[Terrarium::KNOB_2], 0.0f, 1.0f, ::daisy::Parameter::LINEAR);
+    state.mainOut.Init(hw.knob[Terrarium::KNOB_3], 0.0f, 1.0f, ::daisy::Parameter::LINEAR);
+    state.diffusion.Init(hw.knob[Terrarium::KNOB_4], 0.0f, 1.0f, ::daisy::Parameter::LINEAR);
+    state.tapDecay.Init(hw.knob[Terrarium::KNOB_5], 0.0f, 1.0f, ::daisy::Parameter::LINEAR);
+    state.delayTime.Init(hw.knob[Terrarium::KNOB_6], 0.0f, 1.0f, ::daisy::Parameter::LINEAR);
 
-    dryOut.Init(hw.knob[Terrarium::KNOB_1], 0.0f, 1.0f, ::daisy::Parameter::LINEAR);
-    earlyOut.Init(hw.knob[Terrarium::KNOB_2], 0.0f, 1.0f, ::daisy::Parameter::LINEAR);
-    mainOut.Init(hw.knob[Terrarium::KNOB_3], 0.0f, 1.0f, ::daisy::Parameter::LINEAR);
-    diffusion.Init(hw.knob[Terrarium::KNOB_4], 0.0f, 1.0f, ::daisy::Parameter::LINEAR); 
-    tapDecay.Init(hw.knob[Terrarium::KNOB_5], 0.0f, 1.0f, ::daisy::Parameter::LINEAR); 
-    delayTime.Init(hw.knob[Terrarium::KNOB_6], 0.0f, 1.0f, ::daisy::Parameter::LINEAR); 
+    // Initialize previous parameter values
+    state.prevDryOut = 0.0f;
+    state.prevEarlyOut = 0.0f;
+    state.prevMainOut = 0.0f;
+    state.prevDelayTime = 0.0f;
+    state.prevDiffusion = 0.0f;
+    state.prevTapDecay = 0.0f;
+    state.prevNumDelayLines = 1.0f; // Start with 1 delay line (no switches pressed)
 
-    pdryout_value = 0.0;
-    pearlyout_value = 0.0;
-    pmainout_value = 0.0;
-    ptime_value = 0.0;
-    pdiffusion_value = 0.0;
-    ptap_decay_value = 0.0;
-    pnumDelayLines = 5.0; // Set to max number of delay lines initially
+    // Initialize state
+    state.currentPreset = 0;
+    state.bypass = true;
 
-    // Init the LEDs and set activate bypass
-    led1.Init(hw.seed.GetPin(Terrarium::LED_1),false);
-    led1.Update();
-    bypass = true;
+    // Initialize LEDs
+    state.led1.Init(hw.seed.GetPin(Terrarium::LED_1), false);
+    state.led1.Update();
 
-    led2.Init(hw.seed.GetPin(Terrarium::LED_2),false);
-    led2.Update();
+    state.led2.Init(hw.seed.GetPin(Terrarium::LED_2), false);
+    state.led2.Update();
 
+    // Start audio processing
     hw.StartAdc();
     hw.StartAudio(AudioCallback);
-    while(1)
-    {
-        // Do Stuff Infinitely Here
+
+    while (1) {
         System::Delay(10);
     }
 }
